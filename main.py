0a6615bec4e5f1efceba79f523a1fc74ced73672
daniel.silva <50593453+DanielTeofilo97@users.noreply.github.com>
2023-06-14 09:34:13 -0300

    init commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
+
+        return redirect('/home')
+    
+    return render_template('login.html')
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        data_aniversario = request.form['data_aniversario']
+        senha = request.form['senha']
+
+        return redirect('/home')
+
+    return render_template('cadastro.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
